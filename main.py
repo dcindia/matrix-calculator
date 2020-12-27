e1a5ddf80a0a79c54581962177eb5cc5950821d8
darpan <darpan5552@gmail.com>
2020-12-27 13:48:54 +0530

    New Refreshed Interface Based on Material Design-More Colourful & dynamic Added capability for multiple errors Added New Fonts 	new file:   fonts/Alata-Regular.ttf 	new file:   fonts/BalooBhai2-Regular.ttf 	new file:   fonts/BioRhyme-Bold.ttf 	new file:   fonts/ChakraPetch-SemiBoldItalic.ttf 	new file:   fonts/Commissioner-Medium.ttf
    Deleted Old Fonts
    	deleted:    fonts/Azonix.ttf
    	deleted:    fonts/Digital7Monoitalic.ttf
    	deleted:    fonts/ERASDEMI.ttf
    	deleted:    fonts/ERASMD.ttf

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # All the import statements needed in current version or upcoming version
 import kivy
 from kivy.metrics import sp
-from kivy.properties import StringProperty
-
-kivy.require('1.9.1')
+from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivymd.app import MDApp
+from kivy.utils import get_color_from_hex
 from kivy.config import Config
 from kivy.core.window import Window
 import re
 from fractions import Fraction
+
+kivy.require('1.9.1')
 
 # // Search for resources in project root file (optional)
 Config.write()
@@ -20,33 +21,31 @@ kivy.resources.resource_add_path("./")
 # // Added to fix text-box hidden behind keyboard
 Window.softinput_mode = 'below_target'
 
+# Window.size = (450, 750)
+
 
 class MatrixValue(TextInput):
     def __init__(self, **kwargs):
         super(MatrixValue, self).__init__(**kwargs)
+        # self.size_hint_y = None
+        # self.row_default_height = 30
+        # self.height = 60
+        self.background_normal = ''
         self.multiline = False
-        self.input_type = 'number'
-        self.font_size = sp(25)
-        self.font_family = 'bold'
-        self.background_color = [0, 0.6, 0.3, 0.5]
+        self.font_size = sp(20)
+        self.background_color = get_color_from_hex('#69B6BA')
         self.foreground_color = [1, 1, 0.8, 1]
 
 
 # Development of grid layout that contains all the units of matrix
 class MatrixGrid(GridLayout, BoxLayout):
-    order = StringProperty('')
+    order = NumericProperty(0)
 
-    # Function to make determinant as per the provided order
-    def build_matrix(self, *args):
-        error = Validator.chk_order(self.order)
-        if error:
-            self.parent.ids.error_box.text = error
-            return
-        else:
-            self.parent.ids.error_box.text = ''
+    # Function to make Matrix view as per the provided order
+    def on_order(self, *args):
 
         order = int(self.order)
-
+        MDApp.get_running_app().root.ids.display_box.text = ''
         self.clear_widgets()
         self.cols = order
         self.rows = order
@@ -54,27 +53,24 @@ class MatrixGrid(GridLayout, BoxLayout):
         for i in range(1, order + 1):
             for k in range(1, order + 1):
                 set_id = 'a' + str(i) + str(k)
-                text_input = MatrixValue(id=set_id)
+                text_input = MatrixValue()
+                text_input.id = set_id
                 self.add_widget(text_input)
 
-    def on_order(self, *args):
-        self.build_matrix(self)
 
-
-### Our main App
-### All the initiations, exchanges and processes done here
-class matrixCalculator(MDApp):
+# // Our main App
+# // All the initiations, exchanges and processes done here
+class MatrixCalculator(MDApp):
     def __init__(self, **kwargs):
         self.title = "Matrix Calculator"
         self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "Gray"
         super().__init__(**kwargs)
 
-    # Convert list of matrix values into rows and column type matrix
+    # Convert input_matrix into nested lists
     def matrix_builder(self, values_list):
         Mvalues_list = []
         temp_list = []
-        order = int(self.root.ids.order_input.text)
+        order = int(self.root.ids.input_matrix.order)
 
         for i in range(order):
             for k in range(order):
@@ -92,15 +88,23 @@ class matrixCalculator(MDApp):
         if not children_list:  # // Checks that calculation not done on empty set of values
             return "---"
         values_list = []  # // List of all valid values user entered
+        error_list = []
         for child in children_list: # // Checks and Fetches all units of matrix one-by-one
             error = Validator.chk_value(child.text)
-            if error:
-                self.root.ids.error_box.text = error # // Prints Error message
-                return "----"
-            else:
-                values_list.append(Fraction(child.text).limit_denominator(999)) # Value converted to fraction
-        else:
-            self.root.ids.error_box.text = '' # // Removes error message when all values checked
+
+            if error and error not in error_list:
+                error_list.append(error)
+            elif not error:
+                values_list.append(Fraction(child.text).limit_denominator(999))  # Value converted to fraction
+        else:  # After checking all values for validity
+            if len(error_list) != 0:  # If error present
+                if len(error_list) > 4: error_list.insert(3, '. . .')  # Add 3 dots if more than 4 errors
+                error_list = error_list[0:4]  # Display max. 4 errors at a time
+                error_string = '\n'.join(error_list)
+                self.root.ids.display_box.text = error_string
+                return "---"
+            else:  # If all values are complete and error free
+                self.root.ids.display_box.text = ''  # Removes error message when all values verified
 
         # // Covert Linear List to Matrix-type Nested List
         values_list.reverse()
@@ -109,8 +113,8 @@ class matrixCalculator(MDApp):
         # // Passes the matrix to calculate Determinant
         answer = Calculator.determinant(matrix_list)
 
-        # // Sets the answer to show_answer label
-        self.root.ids.show_answer.text = str(answer)
+        # // Sets the answer to display_box
+        self.root.ids.display_box.text = "[b]Answer:    [/b]" + str(answer)
 
     # //// Sets the root of our window
     def build(self):
@@ -129,15 +133,15 @@ class Calculator:
 
         indices = list(range(len(A)))
 
-        # Section 2: when at 2x2 submatrices recursive calls end
+        # Section 2: when at 2x2 sub-matrices recursive calls end
         if len(A) == 2 and len(A[0]) == 2:
             val = A[0][0] * A[1][1] - A[1][0] * A[0][1]
             return val
 
-        # Section 3: define submatrix for focus column and 
+        # Section 3: define sub-matrix for focus column and
         #      call this function
         for fc in indices:  # A) for each focus column, ...
-            # find the submatrix ...
+            # find the sub-matrix ...
             As = list(A)  # B) make a copy, and ...
             As = As[1:]  # ... C) remove the first row
             height = len(As)  # D)
@@ -148,7 +152,7 @@ class Calculator:
                 As[i] = As[i][0:fc] + As[i][fc + 1:]
 
             sign = (-1) ** (fc % 2)  # F)
-            # G) pass submatrix recursively
+            # G) pass sub-matrix recursively
             sub_det = Calculator.determinant(As)
             # H) total all returns from recursion
             total += sign * A[0][fc] * sub_det
@@ -157,28 +161,9 @@ class Calculator:
 
 
 # *****************************************************************************
-# *****************************************************************************
-# *****************************************************************************
 # ////// Class dedicated to verify user inputs
 class Validator:
 
-    def chk_order(order):
-        error = None
-        order = order.strip()
-
-        try:
-            # // Raises Exception if any of below to conditions are not satisfied
-            order = int(order)
-            if order == '':
-                raise Exception
-            elif not (0 < order < 5):
-                raise Exception
-        except:
-            error = "Order must be single digit in range 1 to 4."
-
-        # // Returns "None" for no errors
-        # // Otherwise specified error statement
-        return error
 
     def chk_value(value):
         value = re.sub(r"\s", "", value)  # // Removes all types of whitespaces
@@ -190,25 +175,25 @@ class Validator:
         # // If false, then do some pre-tests to find exact problem
         if not re.match(master_pattern, value):
             if value == '':
-                error = "Any part of matrix can't be left EMPTY."
+                error = "! Any part of matrix can't be EMPTY."
             elif re.search(r"[^\+\-\.\/0-9]", value):
-                error = "Invalid characters in one/more values."
+                error = "! Invalid characters in one/more values."
             elif len(re.findall(r"[\/]", value)) > 1:
-                error = "More than one \'/\' in single value not allowed."
+                error = "! Multiple \'/\' in single value not allowed."
             elif re.search(r"[\/](\+|\-)", value):
-                error = "+/-  can only be in Numerator, NOT in Denominator."
+                error = "! +/- can be in Numerator, NOT in Denominator."
             elif re.match(r"^((\+|\-)?\d{1,3}([\.]\d)?[\/](\+|\-)?\d{1,3}([\.]\d)?)$", value):
-                error = "Both decimal and fraction can't be in a single value."
+                error = "! Decimal and Fraction can't be together."
             elif re.search(r"\d{4,}", value):
-                error = "Any Numerical part can't hold more than 3 digits."
+                error = "! Max. 3 digits allowed for numerical part."
             else:
-                error = "Improper structure of entered value/s."
+                error = "! Improper structure of entered value/s."
 
         # // Returns "None" for no errors
         # // Otherwise specified error statement
         return error
 
 
-### Driver needed to self start the function ---- VROOM! VROOM!
+# /// Driver needed to self start the function ---- VROOM! VROOM!
 if __name__ == "__main__":
-    matrixCalculator().run()
+    MatrixCalculator().run()
